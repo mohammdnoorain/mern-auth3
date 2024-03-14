d6a04b297c35eeb994ef36fbcb60a85b5058cb4e
mohammdnoorain <123652138+mohammdnoorain@users.noreply.github.com>
2024-03-14 12:42:47 +0530

    Added Routes and created Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-  
-  <>
-    <h1 className='text-yellow-600'>heyy</h1>
-    <div className='text-red-500'>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
