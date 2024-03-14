7504fa1bbe0738e52bd06d6df3e5e6fb17db3d84
mohammdnoorain <123652138+mohammdnoorain@users.noreply.github.com>
2024-03-14 15:43:03 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import express from 'express';
-const app=express();
-app.listen(3000,()=>{
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+try {
+  
+   await mongoose.connect(process.env.MONGO)
+   console.log("databse connected");
+  
+} catch (error) {
+    console.log(error);
+}
+  
+const app = express();
 
-
-    console.log("server listeniing on port 3000");
-})
+app.listen(3000, () => {
+  console.log("server listeniing on port 3000");
+});
