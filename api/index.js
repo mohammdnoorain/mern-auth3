29344fbc0528a111777b4f80f02640d988fe4786
mohammdnoorain <123652138+mohammdnoorain@users.noreply.github.com>
2024-03-15 12:44:39 +0530

    created tset api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 try {
-  
-   await mongoose.connect(process.env.MONGO)
-   console.log("databse connected");
-  
+  await mongoose.connect(process.env.MONGO);
+  console.log("databse connected");
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
-  
+
 const app = express();
 
 app.listen(3000, () => {
   console.log("server listeniing on port 3000");
 });
+
+app.use("/api/user", userRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,35 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-
-    username:{
-
-
-        type:String,
-        require:true,
-        unique:true,
-
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
     },
-    email:{
-
-
-        type:String,
-        require:true,
-        unique:true,
-        
+    email: {
+      type: String,
+      require: true,
+      unique: true,
     },
-    password:{
-
-
-        type:String,
-        require:true,
-
-        
-    }
-},{timestamps:true}
-
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
 );
 
-
-const User=mongoose.model('User',userSchema); 
+const User = mongoose.model("User", userSchema);
 export default User;
