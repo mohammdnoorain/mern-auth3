bf43cdf6a445b926e0157ccfe7879ab3798fd0ab
mohammdnoorain <123652138+mohammdnoorain@users.noreply.github.com>
2024-03-18 03:30:58 +0530

    created  cookies through token and signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 // console.log(req.body);
+import jwt  from "jsonwebtoken";
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -17,3 +18,26 @@ export const signup = async (req, res,next) => {
  next(error);
   }
 };
+
+export const signin=async(req,res,next)=>{
+
+const {email,password}=req.body;
+try {
+  const validUser=await User.findOne({email});
+  if(!validUser){
+    return next(errorHandler(401,'user not found'));
+  }
+const validPassword=bcryptjs.compareSync(password,validUser.password);
+if(!validPassword) return next(errorHandler(401,'wrong credentials'))
+const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+
+const {password:hashPassword,...rest}=validUser._doc;
+const expiryDate=new Date(Date.now()+360000);
+res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(validUser);
+
+} catch (error) {
+  next(error);
+  
+}
+
+}
